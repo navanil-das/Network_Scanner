704f21cac9aaba44227766adc3acd7560ad150bd
Navanil <160647890+navanil-das@users.noreply.github.com>
2026-03-18 01:58:52 +0530

    Add banner grabbing function using sockets

--- src/banner_grabber.py
+++ src/banner_grabber.py
@@ -1,1 +1,19 @@
+import socket
 
+def grab_banner(target, port):
+    try:
+        sock = socket.socket()
+        sock.settimeout(2)
+        sock.connect((target, port))
+
+        try:
+            banner = sock.recv(1024).decode().strip()
+        except:
+            banner = ""
+
+        sock.close()
+
+        return banner
+
+    except:
+        return ""
