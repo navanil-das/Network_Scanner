a829736b222c7b1ae2d6cd0210a07128e38bc451
Navanil <160647890+navanil-das@users.noreply.github.com>
2026-03-18 02:10:04 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,21 @@
 def parse_ports(port_range):
     start, end = port_range.split("-")
     return range(int(start), int(end) + 1)
-  
+import subprocess
+import platform
+
+def detect_os(target):
+    param = "-n" if platform.system().lower() == "windows" else "-c"
+    command = ["ping", param, "1", target]
+
+    try:
+        output = subprocess.check_output(command).decode()
+
+        if "ttl=64" in output.lower():
+            return "Linux/Unix"
+        elif "ttl=128" in output.lower():
+            return "Windows"
+        else:
+            return "Unknown"
+    except:
+        return "Unknown"
